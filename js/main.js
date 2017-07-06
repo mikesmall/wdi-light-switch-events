a3c642425261ac463468e1d5d634deafd681b7c0
mikesmall <michael.l.a.small@gmail.com>
2017-07-06 14:58:22 -0400

    Got it working, with a self-selected 'stretch'

--- js/main.js
+++ js/main.js
@@ -1,45 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  // When someone hits the button in the switch, remove the current state class (on/off), and apply the opposite class (on/off)
-
-  // Swap the colors of the body's background and text by adding or removing the dark/light class
-
-  // Lastly, change the text in box to say "It's so bright in here!" when it's on, and "Hey, who turned off the lights?" when it's off
-
-  // Start with small steps:
-
-  // Change the status text when the document is ready
   var status = document.querySelector('.status');
 
   status.innerText = 'Document ready!'
 
-  function changeStatusBack() {
-    status.innerText = 'Anyway, bright enough for ya?!'
+  function statusLight() {
+    status.innerText = 'Good morning!'
   };
 
-  setTimeout(changeStatusBack, 1500)
+  setTimeout(statusLight, 1000);
 
-  // Add a click listener to the switch and incorporate the changes listed above in 'Your Assignment'
-  var lightSwitch = document.querySelector('.plate');
+  var plate = document.querySelector('.plate');
+  var button = document.querySelector('button');
+  var body = document.querySelector('body');
 
   function lightsOut(eventObject) {
-    window.alert('You flicked the switch!');
-  }
+    if (this.className === 'switch on') {
+      body.className = 'dark';
+      this.className = 'switch off';
+      status.innerText = 'Lights out!';
+    }
+    else if (this.className === 'switch off') {
+      body.className = 'light';
+      this.className = 'switch on';
+      statusLight();
+    }
+  };
 
-  lightSwitch.addEventListener('click', lightsOut);
+  button.addEventListener('click', lightsOut);
 
-  // Confirm the switch works
-
-  // How do we switch the classes back now?
-
-  // use an if statement to change the text
-
-  // you should be able to click the switch many times and it should switch back and forth between light and dark modes
-
-
-
-
-
-
-
-}); // end of DOMContentLoaded
+});
